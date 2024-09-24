db9f42ef6839a3d6ff76a815f5be8bc3b4c3fd47
m.hurskaya <maryia.hurskaya@sedex.com>
2024-09-24 10:16:02 +0200

    rm

--- demo/src/main/kotlin/com/example/demo/config/KafkaConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.example.demo.config
-
-object KafkaConfig {
-    const val CLIENT_TOPIC = "clients"
-    const val TRANSACTION_TOPIC = "transactions"
-    const val SERVER_CONFIG = "localhost:29092"
-    const val ACKS_CONFIG = "all"
-}
